19c365360fe137c09d0927e99bdb9678656da3f9
Patrick McGuire <insidenothing@gmail.com>
2010-10-21 09:19:09 -0700

    turn disclaimer into link

--- mod_data.php
+++ mod_data.php
@@ -11,7 +11,7 @@ $main = str_replace('<li class="hidden_elem uiUfiLike uiListItem uiListVerticalI
 $main = str_replace('<ul class="uiList uiUfi focus_target fbUfi" data-ft="&#123;&quot;type&quot;:&quot;ufi&quot;&#125;"><li class="uiUfiComments uiListItem uiListVerticalItemBorder hidden_elem"><ul class="commentList"></ul></li></ul></form>','',$main);
 $main = str_replace('href="#"','href="'.$load.'"',$main);
 $main = str_replace('<br />',' ',$main);
-echo $main."<br /><small>DISCLAIMER, DS1: $load</small>";
+echo $main."<br /><small><a href="$load">DISCLAIMER, DS1: $load</a></small>";
 }
 if ($load2){
 $html = getPage($load2,'JOOMLAmod','5','');
@@ -23,7 +23,7 @@ $main = str_replace('<li class="hidden_elem uiUfiLike uiListItem uiListVerticalI
 $main = str_replace('<ul class="uiList uiUfi focus_target fbUfi" data-ft="&#123;&quot;type&quot;:&quot;ufi&quot;&#125;"><li class="uiUfiComments uiListItem uiListVerticalItemBorder hidden_elem"><ul class="commentList"></ul></li></ul></form>','',$main);
 $main = str_replace('href="#"','href="'.$load2.'"',$main);
 $main = str_replace('<br />',' ',$main);
-echo $main."<br /><small>DISCLAIMER, DS2: $load2</small>";
+echo $main."<br /><small><a href="$load2">DISCLAIMER, DS2: $load2</a></small>";
 }
 if ($load3){
 $html = getPage($load3,'JOOMLAmod','5','');
@@ -35,6 +35,6 @@ $main = str_replace('<li class="hidden_elem uiUfiLike uiListItem uiListVerticalI
 $main = str_replace('<ul class="uiList uiUfi focus_target fbUfi" data-ft="&#123;&quot;type&quot;:&quot;ufi&quot;&#125;"><li class="uiUfiComments uiListItem uiListVerticalItemBorder hidden_elem"><ul class="commentList"></ul></li></ul></form>','',$main);
 $main = str_replace('href="#"','href="'.$load3.'"',$main);
 $main = str_replace('<br />',' ',$main);
-echo $main."<br /><small>DISCLAIMER, DS3: $load3</small>";
+echo $main."<br /><small><a href="$load3">DISCLAIMER, DS3: $load3</a></small>";
 }
 require(JModuleHelper::getLayoutPath('mod_data'));
